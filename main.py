f4b9e835194c68b1d4d34ba1c5774c59369f655c
barabarinov <barabarinov@gmail.com>
2024-04-16 10:20:05 +0300

    Deleted comments

--- main.py
+++ main.py
@@ -19,20 +19,17 @@ def get_db():
         db.close()
 
 
-# Create an author
 @app.post("/authors/", response_model=schemas.Author)
 def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
     return crud.create_author(db=db, author=author)
 
 
-# Get a list of authors
 @app.get("/authors/", response_model=list[schemas.Author])
 def read_authors(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     authors = crud.get_authors(db, skip=skip, limit=limit)
     return authors
 
 
-# Get a single author by ID
 @app.get("/authors/{author_id}/", response_model=schemas.Author)
 def read_author(author_id: int, db: Session = Depends(get_db)):
     db_author = crud.get_author(db, author_id=author_id)
@@ -41,20 +38,17 @@ def read_author(author_id: int, db: Session = Depends(get_db)):
     return db_author
 
 
-# Create a new book for a specific author
 @app.post("/books/", response_model=schemas.Book)
 def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
     return crud.create_book(db=db, book=book)
 
 
-# Get a list of books
 @app.get("/books/", response_model=list[schemas.Book])
 def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     books = crud.get_books(db, skip=skip, limit=limit)
     return books
 
 
-# Filter books by author ID
 @app.get("/books/author/{author_id}/", response_model=list[schemas.Book])
 def read_books_by_author(author_id: int, db: Session = Depends(get_db)):
     return db.query(models.Book).filter(models.Book.author_id == author_id).all()
